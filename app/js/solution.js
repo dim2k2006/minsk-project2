1e18eee5835d386ebd046513eba18b1009db1415
Дмитрий Малофеев <dim2k2006@yandex.ru>
2016-02-19 02:27:00 +0300

    Второй рабочий прототип

--- app/js/solution.js
+++ app/js/solution.js
@@ -15,109 +15,100 @@
     function solution(maze, x, y) {
         // todo: построить правильный маршрут к выходу
         var walker = {
-            cycle: 0,
-            limit: 134,
             min: [0, 0],
-            max: [maze[0].length, maze.length],
+            max: [maze[0].length, maze.length - 1],
             currentX: 1,
             currentY: 0,
             direction: 0,
             path: [],
-            exit: false,
             findWall: function () {
-                // if (this.cycle < this.limit) {
-                    // this.cycle = this.cycle + 1;
+                // Ищет стену справа (взависимости от направления движения)
 
-                    
-                    // Ищет стену справа (взависимости от направления движения)
-                    // console.log('');
-                    // console.log('/----Найти стену---/');
+                // Двигаюсь вперед, стена справа
+                if (this.direction === 0) {
+                    if (maze[this.currentY][this.currentX - 1] !== -1) {
+                        this.currentX = this.currentX - 1;
+                        this.path.push([this.currentX, this.currentY]);
 
-                    // Двигаюсь вперед, стена справа
-                    if (this.direction === 0) {
-                        if (maze[this.currentY][this.currentX - 1] !== -1) {
-                            this.currentX = this.currentX - 1;
-                            this.path.push([this.currentX, this.currentY]);
+                        // console.log('Ищу стену справа');
 
-                            // console.log('Ищу стену справа');
-
-                            // Если вышел на угол, движение назад
-                            if (maze[this.currentY - 1][this.currentX] === 0) {
-                                this.currentY = this.currentY - 1;
-                                this.path.push([this.currentX, this.currentY]);
-                                this.direction = 180;
-                            }
-
-                            this.findWall();
-                        } else {
-                            // console.log('Стена справа');
-                            // console.log('/----Найти стену---/');
-                            this.getDirection();
-                        }
-                    }
-
-                    // Двигаюсь вправо, стена снизу
-                    if (this.direction === 90) {
-                        if (maze[this.currentY - 1][this.currentX] !== -1) {
+                        // Если вышел на угол, движение назад
+                        if (maze[this.currentY - 1][this.currentX] === 0) {
                             this.currentY = this.currentY - 1;
                             this.path.push([this.currentX, this.currentY]);
-
-                            // console.log('Ищу стену снизу');
-
-                            // Если вышел на угол, движение назад
-                            if (maze[this.currentY - 1][this.currentX] === 0) {
-                                this.currentY = this.currentY - 1;
-                                this.path.push([this.currentX, this.currentY]);
-                                this.direction = 180;
-                            }
-
-                            this.findWall();
-                        } else {
-                            // console.log('Стена снизу');
-                            // console.log('/----Найти стену---/');
-                            this.getDirection();
+                            this.direction = 180;
                         }
-                    }
 
-                    // Двигаюсь назад, стена слева
-                    if (this.direction === 180) {
-                        if (maze[this.currentY][this.currentX + 1] !== -1) {
-                            this.currentX = this.currentX + 1;
+                        this.findWall();
+                    } else {
+                        // console.log('Стена справа');
+                        // console.log('/----Найти стену---/');
+                        this.getDirection();
+                    }
+                }
+
+                // Двигаюсь вправо, стена снизу
+                if (this.direction === 90) {
+                    if (maze[this.currentY - 1][this.currentX] !== -1) {
+                        this.currentY = this.currentY - 1;
+                        this.path.push([this.currentX, this.currentY]);
+
+                        // console.log('Ищу стену снизу');
+
+                        // Если вышел на угол, движение назад
+                        if (maze[this.currentY - 1][this.currentX] === 0) {
+                            this.currentY = this.currentY - 1;
                             this.path.push([this.currentX, this.currentY]);
-
-                            // console.log('Ищу стену слева');
-
-                            // Если вышел на угол, движение вперед
-                            if (maze[this.currentY + 1][this.currentX] === 0) {
-                                this.currentY = this.currentY + 1;
-                                this.path.push([this.currentX, this.currentY]);
-                                this.direction = 0;
-                            } 
-                            
-                            this.findWall();
-                        } else {
-                            // console.log('Стена слева');
-                            // console.log('/----Найти стену---/');
-                            this.getDirection();
+                            this.direction = 180;
                         }
-                    }
 
-                    // Двигаюсь влево, стена спереди
-                    if (this.direction === 270) {
+                        this.findWall();
+                    } else {
+                        // console.log('Стена снизу');
+                        // console.log('/----Найти стену---/');
+                        this.getDirection();
+                    }
+                }
+
+                // Двигаюсь назад, стена слева
+                if (this.direction === 180) {
+                    if (maze[this.currentY][this.currentX + 1] !== -1) {
+                        this.currentX = this.currentX + 1;
+                        this.path.push([this.currentX, this.currentY]);
+
+                        // console.log('Ищу стену слева');
+
+                        // Если вышел на угол, движение вперед
+                        if (maze[this.currentY + 1][this.currentX] === 0) {
+                            this.currentY = this.currentY + 1;
+                            this.path.push([this.currentX, this.currentY]);
+                            this.direction = 0;
+                        } 
+                        
+                        this.findWall();
+                    } else {
+                        // console.log('Стена слева');
+                        // console.log('/----Найти стену---/');
+                        this.getDirection();
+                    }
+                }
+
+                // Двигаюсь влево, стена спереди
+                if (this.direction === 270) {
+                    if (this.currentY + 1 <= this.max[1]) {
                         if (maze[this.currentY + 1][this.currentX] !== -1) {
                             this.currentY = this.currentY + 1;
                             this.path.push([this.currentX, this.currentY]);
 
                             // console.log('Ищу стену спереди');
 
-                            // Проверить на выход
-                            this.ifExit();
-
                             // Если вышел на угол, движение вперед
-                            if (maze[this.currentY + 1][this.currentX] === 0) {
-                                this.currentY = this.currentY + 1;
-                                this.path.push([this.currentX, this.currentY]);
-                                this.direction = 0;
+                            if (this.currentY + 1 <= this.max[1]) {
+                                if (maze[this.currentY + 1][this.currentX] === 0) {
+                                    this.currentY = this.currentY + 1;
+                                    this.path.push([this.currentX, this.currentY]);
+                                    this.direction = 0;
+                                }
                             }
 
                             this.findWall();
@@ -126,11 +117,11 @@
                             // console.log('/----Найти стену---/');
                             this.getDirection();
                         }
+                    } else {
+                        console.log('Выход');
+                        return;
                     }
-                // } else {
-                    // console.log('Превышен лимит запусков');
-                    // return false;
-                // }
+                }
             },
             getDirection: function () {
                 // Двигаюсь вперед
@@ -164,48 +155,31 @@
                 this.walk();
             },
             walk: function () {
-                if (this.cycle < this.limit) {
-                    this.cycle = this.cycle + 1;
-
-                    // Иду вперед
-                    if (this.direction === 0) {
-                        this.currentY = this.currentY + 1;
-                        this.path.push([this.currentX, this.currentY]);
-                    }
-
-                    // Иду вправо
-                    if (this.direction === 90) {
-                        this.currentX = this.currentX - 1;
-                        this.path.push([this.currentX, this.currentY]);
-                    }
-
-                    // Иду назад
-                    if (this.direction === 180) {
-                        this.currentY = this.currentY - 1;
-                        this.path.push([this.currentX, this.currentY]);
-                    }
-
-                    // Иду Влево
-                    if (this.direction === 270) {
-                        this.currentX = this.currentX + 1;
-                        this.path.push([this.currentX, this.currentY]);
-                    }
-
-                    console.log('-------------');
-                    console.log(this.currentX);
-                    console.log(this.currentY);
-                    this.findWall();
-                } else {
-                    console.log('Превышен лимит запусков');
-                    return false;
-                }   
-            },
-            ifExit: function () {
-                // Проверка на выход
-                if (this.currentY + 1 === this.max[1]) {
-                    this.exit = true;
-                    console.log('Выход');
+                // Иду вперед
+                if (this.direction === 0) {
+                    this.currentY = this.currentY + 1;
+                    this.path.push([this.currentX, this.currentY]);
                 }
+
+                // Иду вправо
+                if (this.direction === 90) {
+                    this.currentX = this.currentX - 1;
+                    this.path.push([this.currentX, this.currentY]);
+                }
+
+                // Иду назад
+                if (this.direction === 180) {
+                    this.currentY = this.currentY - 1;
+                    this.path.push([this.currentX, this.currentY]);
+                }
+
+                // Иду Влево
+                if (this.direction === 270) {
+                    this.currentX = this.currentX + 1;
+                    this.path.push([this.currentX, this.currentY]);
+                }
+
+                this.findWall();  
             },
             init: function () {
                 this.findWall();
